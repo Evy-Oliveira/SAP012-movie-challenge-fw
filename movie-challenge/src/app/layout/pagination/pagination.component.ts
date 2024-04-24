eb91d3c77877d4162ba64f21c38f9f3291105240
Evy-Oliveira <evelyn.o-s@hotmail.com>
2024-04-24 14:19:32 -0300

    pagination, queryParams e activatedRoute

--- movie-challenge/src/app/layout/pagination/pagination.component.ts
+++ movie-challenge/src/app/layout/pagination/pagination.component.ts
@@ -1,20 +1,29 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnInit{
+  constructor(){
+    this.updateTotalPagesArray();
+  }
 
   @Input() currentPage:number = 0;
   @Input() totalPages: number = 0;
   @Output() onSelectPage: EventEmitter<number> = new EventEmitter<number>();
+  totalPagesArray: number[] = [];
 
-  get totalPagesArray(): number[]{
-    return Array.from({length:this.totalPages}, (_, index) => index + 1);
+  ngOnInit(): void {
+    // this.updateTotalPagesArray();
   }
-
+  ngOnChanges(): void{
+    this.updateTotalPagesArray();
+  }
+  updateTotalPagesArray(): void{
+    this.totalPagesArray = Array.from({length:this.totalPages}, (_, index) => index + 1)
+  }
   selectPage(page: number){
     this.onSelectPage.emit(page)
   }
@@ -22,9 +31,12 @@ export class PaginationComponent {
     return this.currentPage === page;
   }
   isPreviousDisabled(): boolean{
-    return this.currentPage === 1;
+    return this.currentPage <= 1;
   }
   isNextDisabled(): boolean{
-    return this.currentPage === this.totalPages;
+    return this.currentPage >= this.totalPages;
+  }
+  trackByFn(index: number, item: number){
+    return index;
   }
 }
